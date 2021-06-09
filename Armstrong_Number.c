7ed7875e5864c581fe52b5815f8c6889d0d4097d
Dorayya Chowdary <80533487+Dorayya-chowdary@users.noreply.github.com>
2021-06-09 08:40:50 +0530

    Add files via upload

---
+++ Armstrong_Number.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int num,dc=0,a,b=0,res=0,rem;//dc=Digit Count
+	scanf("%d",&num);
+	a=num;
+	while(a)
+	{
+		a=a/10;
+		dc++;
+	}
+	a=num;
+	while(a)
+	{
+		rem=a%10;	    
+		a=a/10;
+		res=res+pow(rem,dc);
+	}
+	if(num==res)
+	{
+		printf("the given number %d is Armstrong Number",num);
+	}
+	else
+	{
+		printf("the given number %d is not Armstrong Number",num);
+		
+	}
+
+	
+	
+}
