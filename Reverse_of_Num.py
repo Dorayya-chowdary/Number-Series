5864a00852930b9a8965d4d3a96ac81ca7608aab
Dorayya Chowdary <80533487+Dorayya-chowdary@users.noreply.github.com>
2021-06-12 21:52:38 +0530

    Update Reverse_of_Num.py

--- Reverse_of_Num.py
+++ Reverse_of_Num.py
@@ -1,9 +1,8 @@
 def reverse(num):
-    a=num
     rev=0
-    while a:
+    while num:
         rem=a%10
-        a=a//10
+        num=num//10
         rev=rev*10+rem
     return rev
 num=int(input('Enter the number'))
